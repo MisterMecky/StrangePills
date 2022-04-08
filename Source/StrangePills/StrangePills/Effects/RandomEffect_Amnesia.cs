e34e914851fd9de3701b8468e1f1f74996842a24
MisterMecky <62370007+MisterMecky@users.noreply.github.com>
2022-04-09 02:17:00 +0800

    more work

---
+++ Source/StrangePills/StrangePills/Effects/RandomEffect_Amnesia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace StrangePills
+{
+    public class RandomEffect_Amnesia : RandomEffect
+    {
+        public override bool TryDoEffect(Pawn pawn)
+        {
+            List<Thought_Memory> memories = pawn.needs.mood.thoughts.memories.Memories;
+            foreach(var m in memories)
+            {
+                pawn.needs.mood.thoughts.memories.RemoveMemory(m);
+            }
+            return true;
+        }
+    }
+}
